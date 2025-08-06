dc0fd04bf38b53d4c5b310b7991b20db09ce0461
Viet Anh Khoa Tran <v.tran@fz-juelich.de>
2025-08-06 10:54:19 +0200

    telescope and tags

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,31 +6,30 @@ return {
 		keys = {
 			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
 			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
-			{ "<leader>fp", "<cmd>Telescope live_grep glob_pattern=*.py<cr>", desc = "Live Grep (Only python files)" },
 			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
-			{ "<leader>fl", "<cmd>Telescope lsp_workspace_symbols<cr>", desc = "Help Tags" },
+			{ "<leader>ft", "<cmd>Telescope tags<cr>", desc = "Tags" },
 		},
-		opts = {
-			extensions = {
-				fzy_native = {
-					override_generic_sorter = false,
-					override_file_sorter = true,
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					fzy_native = {
+						override_generic_sorter = true,
+						override_file_sorter = true,
+					},
 				},
-			},
-			pickers = {
-				live_grep = {
-					file_ignore_patterns = { ".git" },
-					additional_args = function(opts)
-						return { "--hidden", "--no-ignore" }
-					end,
+				pickers = {
+					live_grep = {
+						file_ignore_patterns = { ".git" },
+						additional_args = function(opts)
+							return { "--hidden", "--no-ignore" }
+						end,
+					},
 				},
-			},
-		},
+			})
+			require("telescope").load_extension("fzy_native")
+		end,
 	},
 	{
 		"nvim-telescope/telescope-fzy-native.nvim",
-		setup = function()
-			require("telescope").load_extension("fzy_native")
-		end,
 	},
 }
