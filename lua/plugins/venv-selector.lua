69654032cb69583bbfeee653597e5f8f891ecf5b
Viet Anh Khoa Tran <v.tran@fz-juelich.de>
2025-09-03 14:55:26 +0200

    again

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -5,7 +5,6 @@ return {
 			"neovim/nvim-lspconfig",
 		},
 		lazy = false,
-		branch = "regexp", -- This is the regexp branch, use this for the new version
 		keys = {
 			{ ",v", "<cmd>VenvSelect<cr>" },
 		},
